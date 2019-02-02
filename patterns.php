4cd71a62a2cb46772c0f6ee78c7ed946e2511ef4
MaxiM <mmirrev@mail.ru>
2019-02-03 01:30:03 +0300

    03/02/19

--- patterns.php
+++ patterns.php
@@ -1757,7 +1757,7 @@ try {
 
 
 
-#################### Класс ReflectionFunctionAbsract ####################
+#################### Класс ReflectionFunction ####################
 
 // Класс ReflectionFunction сообщает информацию о функциях.
 
@@ -1804,3 +1804,173 @@ function foo4() {}
 // Создание экземпляра класса ReflectionFunction
 $reflect = new ReflectionFunction('foo1');
 echo $reflect;
+
+foreach($reflect->getParameters() as $i => $param) {
+	printf(
+		"--Параметр #%d: %s{\n".
+		"	Допуск NULL: %s\n".
+		"	Передан по ссылке: %s\n".
+		"	Обязательный?: %s\n".
+		"}\n",
+		$i,
+		$param->getName(),
+		var_export($param->allowsNull(), 1),
+		var_export($param->isPassedByReference(), 1),
+		$param->isOptional() ? 'нет' : 'да'
+	);
+}
+
+
+abstract class MyClass
+{
+	public $a = 1;
+	protected $b = 2;
+	private $c = 3;
+	public static $cnt = 0;
+	const HANDS = 2;
+	abstract function foo();
+	
+	public static function bar()
+	{
+		// что делаем
+	}
+
+	public function sayHello($name, $h = '1')
+	{
+		static $count = 1;
+		return "<h$h>Hello, $name</h$h>";
+	}
+}
+
+// Обзор пользовательского класса
+Reflection::export(new ReflectionClass('MyClass'));
+
+// Обзор встроенного класса
+Reflection::export(new ReflectionClass('Exception'));
+
+
+interface MyInterface{}
+
+class Object{}
+
+class Counter extends Object implements MyInterface
+{
+	const START = 0;
+	private static $c = Counter::START;
+
+	public function count()
+	{
+		return self::$c++;
+	}
+}
+
+
+// Создание экземпляра класса ReflectionClass
+$class = new ReflectionClass('Counter');
+
+printf(
+	"===> %s%s%s %s '%s' [экземпляр класса %s]\n".
+	"	объявлен в %s\n".
+	"	строки с %d по %d\n",
+	$class->isInternal() ? 'Встроенный' : 'Пользовательский',
+	$class->isAbstract() ? 'абстракт' : '',
+	$class->isFinal() ? 'финальный' : '',
+	$class->isInterface() ? 'интерфейс' : 'класс',
+	$class->getName(),
+	var_export($class->getParentClass(), 1),
+	$class->getFilename(),
+	$class->getStartLine(),
+	$class->getEndline()
+);
+
+// Вывод тех интерфейсов, которые реализует этот класс
+printf("---> Интерфейсы:\n %s\n", var_export($class->getInterfaces(), 1));
+
+// Вывод констант класса
+printf("---> Константы: %s\n", var_export($class->getConstants(), 1));
+
+// Вывод свойств класса
+printf("---> Свойства: %s\n", var_export($class->getProperties(), 1));
+
+// Вывод методов класса
+printf("---> Методы: %s\n", var_export($class->getMethods(), 1));
+
+
+// Если есть возможность создать экземляр класса, то создаем его
+if ($class->isInstantiable()) {
+	$counter = $class->newInstance();
+
+	echo '---> Создан ли экземляр класса ' . $class->getName() . '? ';
+	echo $class->isInstance($counter) ? 'Да' : 'Нет';
+
+	echo '\n---> Создан ли экземляр класса Object()?';
+	echo $class->isInstance(new Object()) ? 'Да' : 'Нет';
+}
+
+
+#################### Класс ReflectionMethod ####################
+
+class Counter
+{
+	private static $c = 0;
+
+	final public static function increment()
+	{
+		return ++self::$c;
+	}
+}
+
+
+// Создание экземпляра класса ReflectionMethod
+$method = new ReflectionMethod('Counter', 'increment');
+
+// Вывод основной информации
+printf(
+	"===> %s%s%s%s%s%s%s метод '%s' (который является %s)\n".
+	"	объявлен в %s\n".
+	"	строки с %d по %d\n".
+	$metod->isInternal() ? 'Встроенный' : 'Пользовательский',
+	$method->isAbstract() ? 'абстрактный' : '',
+	$method->isFinal() ? 'финальный'
+	$method->isPublic() ? 'public'
+	$method->isPrivate() ? 'private'
+	$method->isProtected() ? 'protected'
+	$method->isStatic() ? 'статический'
+	$method->getName(),
+	$method->isConstructor() ? 'конструктором' : 'обычным методов'
+	$method->getFileName(),
+	$class->getStartLine(),
+	$class->getEndline()
+);
+
+
+// Вывод статических переменных, если они есть
+if ($statics = $method->getStaticVariables()) {
+	printf("---> Статическая переменная: %s\n", var_export($statics, 1));
+	);
+}
+
+// Вызов метода
+printf("---> Результат вызова: ");
+$result = $method->invoke(null); // статический метод передаем с null
+
+
+#################### Класс ReflectionExtension ####################
+
+// Создание экземпляра класса ReflectionProperty
+$ext = new ReflectionException('standart');
+
+
+printf(
+	"Имя : %s\n".
+	"Версия : %s\n".
+	"Функция : [%d] %s\n".
+	"Константы  : [%d] %s\n".
+	"Директивы INI  : [%d] %s\n".
+	"Классы  : [%d] %s\n",
+	$ext->getName(),
+	$ext->getVersion() ? $ext->getVersion() : 'NO_VERSION',
+	sizeof($ext->getFunction()),
+);
+
+
