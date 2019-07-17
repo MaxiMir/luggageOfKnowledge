3e38449b354ff707eb93abd9afe938294e0264eb
MaxiMir <mmirrev@mail.ru>
2019-07-17 23:01:34 +0300

    17/07/19

--- PHP/bitrix.php
+++ PHP/bitrix.php
@@ -1,4 +1,6 @@
 <?
+
+// Глобальные фильтры на всякие случаи жизни https://camouf.ru/blog-note/4717/
 	
 /** Удаляем все кроме:
  * /bitrix
@@ -1158,3 +1160,37 @@ function getSymbolCode($urn)
 }
 
 
+# Слайдер:
+if (CModule::IncludeModule("iblock")) {
+    // ID инфоблока из которого выводим элементы
+    $iblock_id = 11;
+
+    $my_slider = CIBlockElement::GetList (
+        // Сортировка элементов
+        ["ID" => "ASC"],
+        ["IBLOCK_ID" => $iblock_id],
+        false,
+        false,
+        // Перечисляесм все свойства элементов, которые планируем выводить
+        [
+            'ID', 
+            'NAME', 
+            'PREVIEW_PICTURE', 
+            'PREVIEW_TEXT', 
+            'PROPERTY_LIN_PR'
+        ]
+    );
+    while($ar_fields = $my_slider->GetNext())
+    {
+        //Выводим элемент со всеми свойствами + верстка
+        $img_path = CFile::GetPath($ar_fields["PREVIEW_PICTURE"]);
+        echo '<li><a href="'.$ar_fields['PROPERTY_LIN_PR_VALUE'].'">';
+        echo '<h4>'.$ar_fields['NAME']."</h4>";
+        echo "<img src='".$img_path."'/>";
+        echo "<p>".$ar_fields['PREVIEW_TEXT']."'</p>";
+        echo '</a></li>';
+    }
+}
+
+// Если попробуете вывести свойство типа HTML/TEXT получите Array. Для его вывода используйте конструкцию:
+echo htmlspecialcharsBack($ar_fields['PROPERTY_КОД_СВОЙСТВА_VALUE']["TEXT"]);
