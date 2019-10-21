83c94b117acaa1425fd1ab5e98632e4876c170a2
MaxiMir <mmirrev@mail.ru>
2019-10-22 00:25:46 +0300

    22/10/19

--- PHP/PHP-notes.php
+++ PHP/PHP-notes.php
@@ -290,3 +290,311 @@
 	 
 	 //получаем значение по умолчанию
 	 $parameter->getDefaultValue();
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 #@ Пример работы с классами и интерфейсами: #@
+	 /**
+	  * Interface PersonInterface
+	  */
+	 interface PersonInterface // указываем только публичные методы
+	 {
+		  public function get(): string;
+		  public function set(string $name);
+	 }
+	 
+	 /**
+	  * Interface CityInterface
+	  */
+	 interface CityInterface
+	 {
+		  public function addPerson(Person $person);
+		  public function getPerson(): array;
+	 }
+	 
+	 /**
+	  * Class Person
+	  */
+	 class Person implements PersonInterface
+	 {
+		  private $name;
+		 
+		  public function get():string
+		  {
+				return $this->name;
+		  }
+		 
+		  public function set(string $value)
+		  {
+				$this->name = $value;
+		  }
+	 }
+	 
+	 /**
+	  * Class City
+	  */
+	 class City implements CityInterface
+	 {
+		  private $persons = [];
+		 
+		  public function addPerson(Person $person)
+		  {
+				$this->persons[] = $person->get();
+		  }
+		 
+		  public function getPerson():array
+		  {
+				return $this->persons;
+		  }
+	 }
+	 
+	 $personMisha = new Person();
+	 $personMisha->set('Misha');
+	 
+	 $personVanya = new Person();
+	 $personVanya->set('Vanya');
+	 
+	 $city = new City();
+	 $city->addPerson($personMisha);
+	 $city->addPerson($personVanya);
+	 $city->getPerson(); // => [0]=> 'Misha', [1]=>'Vanya'
+
+
+
+
+
+	 /**
+	  * Class BaseModel
+	  */
+	 abstract class BaseModel
+	 {
+		  public function selectAll():string
+		  {
+				return 'SELECT * FROM ' . $this->getTableName();
+		  }
+		  
+		  public function db(string $sql)
+		  {
+				// TODO: db реализацию сделать!
+		  }
+		  
+		  abstract public function getTableName():string; // у потомков реализация данного метода отличается
+	 }
+		
+	 /**
+	  * Class Article
+	  */
+	 class Article extends BaseModel
+	 {
+		  public function getTableName():string
+		  {
+				return 'task';
+		  }
+	 }
+		
+	 $task = new Atricle();
+	 echo $task->selectAll();
+		
+	 
+	 
+	 #@ трейд - механизм, реализующий повторное использование кода. Решает проблему отсутствия множественного наследования.
+	 #
+	 /**
+	  * Class Article
+	  */
+	 class Article
+	 {
+		  public $sql;
+		  
+		  public function executeSql() // реализация методов идентична
+		  {
+				return $this->sql;
+		  }
+	 }
+	 
+	 
+	 /**
+	  * Class User
+	  */
+	 class User
+	 {
+		  public $sql;
+		  
+		  public function executeSql() // реализация методов идентична
+		  {
+				return $this->sql;
+		  }
+	 }
+		
+	 
+	 #@ Реализация метода executeSql в классах индентична, а значит можно использовать трейт:
+	 trait BaseModel
+	 {
+		  public $sql;
+		  
+		  public function executeSql() // реализация методов идентична
+		  {
+				return $this->sql;
+		  }
+		 
+		  public function selectAllFromDB()
+		  {
+				$this->sql = 'SELECT * FROM' . $this->getTableName();
+		  }
+	
+		  abstract public function getTableName(): string;
+	 }
+	 
+	 class Article
+	 {
+		  use BaseModel; // можно перечислить несколько трейтов через ','
+		  
+		  public function getTableName()
+		  {
+				return 'articles';
+		  }
+	 }
+		
+	 class User
+	 {
+		  use BaseModel;
+		  
+		  public function getTableName()
+		  {
+				return 'users';
+		  }
+	 }
+		
+	 $article = new Article();
+	 $article->selectAllFromDB();
+	 echo $article->executeSql(); // => SELECT * FROM articles
+	 
+	 $user = new User();
+	 $user->selectAllFromDB();
+	 echo $user->executeSql(); // => SELECT * FROM users
+
+
+	 
+
+	#@ Exceptions:
+	 class User
+	 {
+		  public $sql;
+		 
+		  public function addUser(string $login, string $password)
+		  {
+				if(strlen($login) < 3 || strlen($login) > 15) {
+					 return false;
+				}
+			 
+				if(strlen($password) < 3 || strlen($password) > 6) {
+					 return false;
+				}
+			 
+				$this->sql = "INSERT INTO users VALUES('', {$login}, {$pasword})";
+			 
+				return true;
+		  }
+	 }
+	 
+	 $user = new User();
+	 $result = $user->addUser('Misha', '1234');
+	 
+	 if ($result === true) {
+		  echo 'user was added';
+	 } else {
+		  echo 'fail';
+	 }
+	 
+	 
+	 // Тоже самое используя Exceptions:
+	 
+	 class User
+	 {
+		  public $sql;
+		 
+		  public function addUser(string $login, string $password)
+		  {
+				if(strlen($login) < 3 || strlen($login) > 15) {
+					 throw new Exception('Wrong login');
+				}
+			 
+				if(strlen($password) < 3 || strlen($password) > 6) {
+					 throw new Exception('Wrong password');
+				}
+			 
+				$this->sql = "INSERT INTO users VALUES('', {$login}, {$pasword})";
+			 
+				return true;
+		  }
+	 }
+	 
+	 try {
+		  $user = new User();
+		  $result = $user->addUser('Misha', '1234');
+		  echo 'user was added';
+	 } catch(Exception $e) {
+		  die('Fail');
+	 }
+	 
+	 
+	  // Чтобы каждый раз не писать конструкцию try/catch лучше создать отдельный класс:
+	 /**
+	 * Class UserException
+	 */
+	 class UserException extends Exception
+	 {
+		
+	 }
+	 
+	 /**
+	  * Class UserLoginException
+	  */
+	 class UserLoginException extends UserException
+	 {
+		  protected $message = 'wrong login';
+	 }
+	 
+	 /**
+	  * Class UserPasswordException
+	  */
+	 class UserPasswordException extends UserException
+	 {
+		  protected $message = 'wrong password';
+	 }
+	 
+	 /**
+	  * Class User
+	  */
+	 class User
+	 {
+		  public $sql;
+		 
+		  public function addUser(string $login, string $password)
+		  {
+				if(strlen($login) < 3 || strlen($login) > 15) {
+					 throw new UserLoginException;
+				}
+			 
+				if(strlen($password) < 3 || strlen($password) > 6) {
+					 throw new UserPasswordException;
+				}
+			 
+				$this->sql = "INSERT INTO users VALUES('', {$login}, {$password})";
+			 
+				return true;
+		  }
+	 }
+	 
+	 try {
+		  $user = new User();
+		  $result = $user->addUser('Misha', '1234');
+		  echo 'user was added';
+	 } catch(UserException $e) {
+		  die($e->getMessage());
+	 }
