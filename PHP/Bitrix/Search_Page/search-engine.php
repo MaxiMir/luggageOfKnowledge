9f1832f418a11381cb9e7728d6a669df22702515
MaxiMir <mmirrev@mail.ru>
2019-11-28 00:23:24 +0300

    28/11/19

--- PHP/Bitrix/Search_Page/search-engine.php
+++ PHP/Bitrix/Search_Page/search-engine.php
@@ -350,7 +350,8 @@
 		  ] = $requestPageData;
 		  
 		  $filterData = ["IBLOCK_ID" => CATALOG_I_BLOCK_ID, "ID" => $productID];
-		  $imageSizeArr = ['width' => 400, 'height' => 400];
+		  
+		  $priceInEur = getEurPrice();
 		  
 		  $result_products = CIBlockElement::GetList(
 			  $sortData,
@@ -386,10 +387,30 @@
 				
 				if ($offerData = $resultOffersDB->GetNext()) {
 					 $price = get_price($offerData["ID"]);
+					 
+					 if (!$price) {
+						  $price = get_price_euro($offerData["ID"]);
+						  
+						  if ($price) {
+								$price = $price * $priceInEur;
+						  }
+					 }
+					 
+					 if ($price) {
+						  $price_offer = true;
+					 }
 				}
 				
 				if (!$price) {
 					 $price = get_price($prodID);
+					 
+					 if (!$price) {
+						  $price = get_price_euro($prodID);
+						  
+						  if ($price) {
+								$price = $price * $priceInEur;
+						  }
+					 }
 				}
 				
 				if ($price) {
@@ -397,33 +418,7 @@
 					 $itemData["PRICE_FORMAT"] = generatePriceBlock($price, $price_offer);
 				}
 				
-				
-				$itemData["IMG"] = false;
-				
-				if ($itemData["DETAIL_PICTURE"]) {
-					 ["src" => $itemData["IMG"]] = CFile::ResizeImageGet($detailPicture, $imageSizeArr, BX_RESIZE_IMAGE_EXACT, true);
-				}
-				
-				if (!$itemData["IMG"]) {
-					 $resultOffersDB = CIBlockElement::GetList(
-						 ["CATALOG_PRICE_3" => "ASC", "CATALOG_PRICE_4" => "ASC"],
-						 [
-							 "IBLOCK_ID" => 29,
-							 "ACTIVE" => "Y",
-							 "PROPERTY_CML2_LINK" => $itemData["ID"],
-							 "!DETAIL_PICTURE" => false
-						 ],
-						 false,
-						 ["nTopCount" => 1],
-						 ["*"]
-					 );
-					 
-					 if ($offerData = $resultOffersDB->GetNext()) {
-						  ["src" => $itemData["IMG"]] = CFile::ResizeImageGet($offerData["DETAIL_PICTURE"], $imageSizeArr, BX_RESIZE_IMAGE_EXACT, true);
-					 } else {
-						  $itemData["IMG"] = '/upload/images/no-photo.png';
-					 }
-				}
+				$itemData["IMG"] = getProductImage($itemData);
 				
 				$itemData["COLORS"] = false;
 				
@@ -475,10 +470,10 @@
 					 }
 				}
 				
-				$productsData[] = $itemData;
+				$arr_products[] = $itemData;
 		  }
 		  
-		  return $productsData;
+		  return $arr_products;
 	 }
 	 
 	 
@@ -503,4 +498,68 @@
 		  }
 		  
 		  return $foundItems;
+	 }
+	 
+	 
+	 /**
+	  * @param $prodID
+	  * @return mixed - относительный путь до изображения товара || false
+	  */
+	 function getOfferImage(int $prodID)
+	 {
+		  $offerImageSrc = false;
+		  $imageSettings = [
+			  ['width' => 400, 'height' => 400],
+			  BX_RESIZE_IMAGE_EXACT,
+			  true
+		  ];
+		  
+		  $result_offers = CIBlockElement::GetList(
+			  ["propertysort_DEFAULT" => "DESC", "CATALOG_PRICE_3" => "ASC", "CATALOG_PRICE_4" => "ASC"],
+			  [
+				  "IBLOCK_ID" => OFFERS_I_BLOCK_ID,
+				  "ACTIVE" => "Y",
+				  "PROPERTY_CML2_LINK" => $prodID,
+				  "!DETAIL_PICTURE" => false
+			  ],
+			  false,
+			  ["nTopCount" => 1],
+			  ["*"]
+		  );
+		  
+		  if (["DETAIL_PICTURE" => $offerDetailImage] = $result_offers->GetNext()) {
+				["src" => $offerImageSrc] = CFile::ResizeImageGet($offerDetailImage, ...$imageSettings);
+		  }
+		  
+		  return $offerImageSrc;
+	 }
+	 
+	 
+	 /**
+	  * @param array $arItem
+	  * @return string - относительный путь до изображения товара
+	  */
+	 function getProductImage(array $arItem): string
+	 {
+		  $imageSrc = false;
+		  $imageSettings = [
+			  ['width' => 400, 'height' => 400],
+			  BX_RESIZE_IMAGE_EXACT,
+			  true
+		  ];
+		  
+		  [
+			  "ID" => $prodID,
+			  "DETAIL_PICTURE" => $detailImage,
+		  ] = $arItem;
+		  
+		  if ($detailImage) {
+				["src" => $imageSrc] = CFile::ResizeImageGet($detailImage, ...$imageSettings);
+		  }
+		  
+		  if (!$imageSrc) {
+				$imageSrc = getOfferImage($prodID);
+		  }
+		  
+		  return $imageSrc ?: '/upload/images/no-photo.png';
 	 }
