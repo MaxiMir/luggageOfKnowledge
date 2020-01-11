9ef3447b24fd1f2538cc08940e309885279a44a6
MaxiMir <mmirrev@mail.ru>
2020-01-11 13:46:32 +0300

    11/1/20

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    "indent": [2, "tab"],
+    "no-tabs": 0
+  },
+};
