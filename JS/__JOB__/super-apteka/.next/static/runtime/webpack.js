d81a4717bc8abea7742ed0ad8186be82fd45afe2
Maxim <mmirrev@gmail.com>
2020-05-10 22:42:58 +0300

    add files

--- JS/__JOB__/super-apteka/.next/static/runtime/webpack.js
+++ JS/__JOB__/super-apteka/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7bec73e338f939a499aa";
+/******/ 	var hotCurrentHash = "40e0815a857f883fb6df";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
