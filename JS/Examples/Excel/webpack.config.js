d3c312c830e336c2747d3992e381916fda432437
Maxim Minchenko <mmirrev@gmail.com>
2020-06-07 10:50:28 +0300

    7/6/20

--- JS/Examples/Excel/webpack.config.js
+++ JS/Examples/Excel/webpack.config.js
@@ -1,20 +1,34 @@
 const path = require('path')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const HTMLWebpackPlugin = require('html-webpack-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin') // очистка папки build
+const HTMLWebpackPlugin = require('html-webpack-plugin') // работа с html шаблоном
+const CopyPlugin = require('copy-webpack-plugin') // копирование файлов/директорий
+const MiniCssExtractPlugin = require('mini-css-extract-plugin') // возможность подключать файлы со стилями
 
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === 'production' // <-> cross-env в package.json
 const isDev = !isProd
 
-const filename = ext => isDev ? `bundle.${ ext }` : `bundle.[hash].${ ext }`
+const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`
+
+const cssLoaders = () => {
+  return [
+    {
+      loader: MiniCssExtractPlugin.loader,
+      options: {
+        hmr: isDev, // hot reload
+        reloadAll: true
+      }
+    },
+    'css-loader',
+    'sass-loader'
+  ]
+}
 
 const jsLoaders = () => {
   const loaders = [
     {
       loader: 'babel-loader',
       options: {
-        presets: ['@babel/preset-env']
+        presets: ['@babel/preset-env'] // содержит последние фишки JS
       }
     }
   ]
@@ -29,7 +43,7 @@ const jsLoaders = () => {
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   mode: 'development',
-  entry: ['@babel/polyfill', './index.js'],
+  entry: ['@babel/polyfill', './index.js'], // вначале добавляем полифиллы
   output: {
     filename: filename('js'),
     path: path.resolve(__dirname, 'dist')
@@ -41,8 +55,8 @@ module.exports = {
       '@core': path.resolve(__dirname, 'src/core')
     }
   },
-  devtool: isDev ? 'source-map' : false,
-  devServer: {
+  devtool: isDev ? 'source-map' : false, // добавление source-map
+  devServer: { // плагин webpack-dev-server с hot reload (указывается в package.json)
     port: 3000,
     hot: isDev
   },
@@ -69,17 +83,7 @@ module.exports = {
     rules: [
       {
         test: /\.s[ac]ss$/i,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              hmr: isDev,
-              reloadAll: true
-            }
-          },
-          'css-loader',
-          'sass-loader'
-        ],
+        use: cssLoaders(),
       },
       {
         test: /\.js$/,
