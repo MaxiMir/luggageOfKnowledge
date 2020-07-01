0fc3b491031e40b4b394136c540bb800fd8808be
Maxim <mmiirev@gmail.com>
2020-07-01 23:57:06 +0300

    1/7/20

--- JS/Notes/OOP.js
+++ JS/Notes/OOP.js
@@ -1,26 +1,4 @@
-// @ ЧТО ТАКОЕ PROTOTYPE:
-// #1 добавляем всем объектам метод sayHello:
-const maxiMir = {
-  name: "Maxim",
-  age: 25,
-  greet: () => {
-    console.log('Greet');
-  }
-};
-
-maxiMir.sayHello(); // => Uncaught TypeError
-
-Object.prototype.sayHello = () => {
-  console.log('Hello!');
-};
-
-// цепочка наследования - объект __proto__
-
-maxiMir.sayHello(); // => Hello!
-
-
-
-// #2 один из вариантов наследования:
+// @ один из вариантов наследования:
 const maxCon = Object.create(maxiMir);
 maxCon.age = 30;
 maxCon.greet(); // Greet
