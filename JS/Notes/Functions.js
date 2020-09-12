5180ab256cd2a60389926997472c7eda8d07319f
Maxim Minchenko <mmirrev@gmail.com>
2020-09-12 18:30:11 +0300

    12/9/20

--- JS/Notes/Functions.js
+++ JS/Notes/Functions.js
@@ -603,7 +603,7 @@ const promises = [
 ]
 
 async function old() {
-  for (const promise of await Promise.all(promises)) { // ждем пока все зарезолвятся затем их выводим в консоли
+  for (const promise of await Promise.all(promises)) { // ждем пока все зарезолвится затем их выводим в консоли
     console.log('Old: ', promise)
   }
 }
@@ -611,9 +611,150 @@ async function old() {
 old()
 
 async function modern() {
-  for await (const promise of promises) { // как только промис зарезолвятся он выведится в консоли
+  for await (const promise of promises) { // как только промис зарезолвится он выведется в консоли
     console.log('New: ', promise)
   }
 }
 
 modern()
+
+
+
+
+/**
+ * Программирование с флагами (императивно)
+ */
+
+const isFizz = n => n % 3;
+
+const isBuzz = n => n % 5;
+
+const isFizzBuzz = n => isFizz(n) && isBuzz(n);
+
+for (let i = 1; i < 100; i++) {
+  switch(true) {
+    case isFizzBuzz(i):
+      console.log('FizzBuzz');
+      break;
+    case isFizz(i):
+      console.log('Fizz');
+      break;
+    case isBuzz(i):
+      console.log('Buzz');
+      break;
+    default:
+      console.log(i);
+  }
+}
+
+/**
+ * Функциональное программирование
+ */
+
+const gen = (n, w) => num => num % n === 0 ? w : '';
+
+const fizz = gen(3, 'Fizz');
+
+const buzz = gen(5, 'Buzz');
+
+[...Array(99).keys()]
+    .map(i => i + 1)
+    .forEach(i => console.log(fizz(i) + buzz(i) || i));
+
+/**
+ * ООП
+ */
+
+/*
+Категории интерфейсов:
+
+                Num(Число)      Tag (Value Object)  <=  Printer
+                    ^                   |
+                    |                   v
+Condition   =>  Strategy    =>         Rule
+                                        |
+                                        v
+                                    Collection
+*/
+
+const MAX_NUM = 100;
+
+class Tag /* implements Value */ {
+  constructor(_value) {
+    this.value = _value;
+  }
+}
+
+class Printer {
+  constructor(_context) {
+    this.context = _context;
+  }
+
+  print() {
+    console.log(this.context.value);
+  }
+}
+
+class DivCondition /* implements Condition, Truthy */ {
+  constructor(_divider) {
+    this.divider = _divider;
+  }
+
+  isTruthy(num) {
+    return num % this.divider === 0;
+  }
+}
+
+class AndStrategy /* implements Strategy, Truthy */ {
+  constructor(_conditionsOrStrategies) {
+    this.conditions = _conditionsOrStrategies;
+  }
+
+  isTruthy(num) {
+    for(let i in this.conditions) {
+      if (!this.conditions[i].isTruthy(num)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
+
+class TagNumRule /* implements Ruke */ {
+  constructor(_tag, _strategy) {
+    this.strategy = _strategy;
+    this.tag = _tag;
+  }
+
+  isSuccess(num) {
+    return this.strategy.isTruthy(num);
+  }
+}
+
+class TagNumRulesCollection /* implements collection */ {
+  constructor(_tags) {
+    this.tags = _tags
+  }
+
+  find(num, defaultValue) {
+    for(let i in this.tags) {
+      if (this.tags[i].isSuccess(num)) {
+        return this.tags[i].tag;
+      }
+    }
+
+    return defaultValue;
+  }
+}
+
+const numTags = new TagNumRulesCollection([
+  new TagNumRule(new Tag('FizzBuzz'), new AndStrategy([new DivCondition(3), new DivCondition(5)])),
+  new TagNumRule(new Tag('Fizz'), new AndStrategy([new DivCondition(3)])),
+  new TagNumRule(new Tag('Buzz'), new AndStrategy([new DivCondition(5)]))
+]);
+
+for(let i = 1; i < MAX_NUM; i++) {
+  new Printer(numTags.find(i, new Tag(i))).print()
+}
+
