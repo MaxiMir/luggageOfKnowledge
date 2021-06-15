17964d9f52b486084208e0aeb88f70f5a3712110
Maxim <minchenko_ma@nlmk.com>
2021-06-15 23:12:31 +0300

    15/6/21

---
+++ JS/React/Examples/JwtAuth/README.md
@@ -0,0 +1,5 @@
+### Подписывайся на TeleGram канал - https://t.me/ulbi_tv
+
+##
+
+### PDF версия урока здесь - https://www.patreon.com/ulbitv
---
+++ JS/React/Examples/JwtAuth/client/src/services/UserService.ts
@@ -0,0 +1,10 @@
+import $api from '../http'
+import { AxiosResponse } from 'axios'
+import { IUser } from '../models/IUser'
+
+export default class UserService {
+	static fetchUsers(): Promise<AxiosResponse<IUser[]>> {
+		return $api.get<IUser[]>('/users')
+	}
+}
+
