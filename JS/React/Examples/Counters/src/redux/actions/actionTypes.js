69b7ce1b55c64b887ea6dddaaee393c552f3ce84
Максим Минченко <minchenko_ma@nlmk.com>
2022-03-13 11:06:14 +0300

    13/3/22

--- JS/React/Examples/Counters/src/redux/actions/actionTypes.js
+++
@@ -1,4 +0,0 @@
-export const ADD = 'ADD'
-export const SUB = 'SUB'
-export const ADD_NUMBER = 'ADD_NUMBER'
-export const ADD2 = 'ADD2'
--- JS/React/Examples/Counters/src/redux/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux'
-
-import counter1 from './reducers/counter1'
-import counter2 from './reducers/counter2'
-
-export default combineReducers({ // combineReducers - объединяет редьюсеры
-	counter1, counter2
-})
